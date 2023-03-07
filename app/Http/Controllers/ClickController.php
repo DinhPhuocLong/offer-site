8dfc5ef97d8e45be7d62aa091f72a923619ed377
DinhPhuocLong <l0978011552l@gmail.com>
2023-03-07 09:18:31 +0100

    Add location library

--- app/Http/Controllers/ClickController.php
+++ app/Http/Controllers/ClickController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Click;
 use Illuminate\Http\Request;
+use Stevebauman\Location\Facades\Location;
 
 class ClickController extends Controller
 {
@@ -13,5 +14,11 @@ class ClickController extends Controller
     }
     public function create(Request $request) {
         $ip = $request->ip();
+        if ($position = Location::get($ip)) {
+            // Successfully retrieved position.
+            return $position->countryName;
+        } else {
+            return 'Failed to ';
+        }
     }
 }
